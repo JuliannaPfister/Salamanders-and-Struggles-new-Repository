18d9c73e4e6869d0e2fdd40b088c8809d25bcee9
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2018-04-18 22:49:51 +1000

    Attacking
    Player attacks. About to code enemies taking damage.

---
+++ SalamanderGame/Assets/Scripts/AttackingTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingTrigger : MonoBehaviour {
+
+	//the damage dealt when attacking an enemy
+	public int dmg = 20;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		//calls a function with whatever is being collided with- ie the enemy
+		//if whatever is hit has the tag "Enemy", then the damage function will be called, causing the enemy to take damage
+
+		if (col.isTrigger != true && col.CompareTag ("Enemy"))
+		{
+			col.SendMessageUpwards ("Damage", dmg);
+		}
+	}
+}
---
+++ SalamanderGame/Assets/Scripts/PlayerAttacking.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacking : MonoBehaviour
+{
+	private bool canAttack = true;
+	private bool attacking = false;
+
+	private float coolDownTimer = 0;
+	public float coolDownLength = 1.0f;
+
+	private float animationTimer = 0;
+	public float animationLength = 0.3f;
+
+	public Collider2D attackTrigger;
+
+	private Animator anim;
+
+	void Awake ()
+	{
+		anim = gameObject.GetComponent<Animator> ();
+		attackTrigger.enabled = false;
+	}
+
+	void Start()
+	{
+		coolDownTimer = coolDownLength;
+		animationTimer = animationLength;
+	}
+
+	void Update ()
+	{
+		coolDownTimer += Time.deltaTime;
+		animationTimer += Time.deltaTime;
+
+		canAttack = coolDownTimer > coolDownLength;
+
+		if (Input.GetKeyDown (KeyCode.Mouse0) && canAttack) {
+			coolDownTimer = 0;
+			animationTimer = 0;
+		}
+
+		attacking = animationTimer < animationLength;
+
+		attackTrigger.enabled = attacking;
+
+		anim.SetBool ("Attacking", attacking);
+
+
+	}
+}
